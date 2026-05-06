fe3719df0dfd0152da9d777a55af81d3320b8e25
Valentina Naranjo <142535660+jhainethv161@users.noreply.github.com>
2026-05-06 11:58:36 -0500

    Refactorizacion

---
+++ ruta_ya/src/main/java/com/ruta_ya/controller/CatalogoController.java
@@ -0,0 +1,40 @@
+package com.ruta_ya.controller;
+
+import com.ruta_ya.dto.EstadoUsuarioResponse;
+import com.ruta_ya.dto.MetodoPagoResponse;
+import com.ruta_ya.service.CatalogoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class CatalogoController {
+
+    private final CatalogoService catalogoService;
+
+    @GetMapping("/estados-usuario")
+    public ResponseEntity<?> getAllEstadosUsuario() {
+        try {
+            List<EstadoUsuarioResponse> estados = catalogoService.getAllEstadosUsuario();
+            return ResponseEntity.ok(estados);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/metodos-pago")
+    public ResponseEntity<?> getAllMetodosPago() {
+        try {
+            List<MetodoPagoResponse> metodos = catalogoService.getAllMetodosPago();
+            return ResponseEntity.ok(metodos);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+}
--- ruta_ya/src/main/java/com/ruta_ya/service/CatalogoService.java
+++ ruta_ya/src/main/java/com/ruta_ya/service/CatalogoService.java
@@ -1,6 +1,8 @@
 package com.ruta_ya.service;
 
+import com.ruta_ya.dto.EstadoUsuarioResponse;
 import com.ruta_ya.dto.MetodoPagoResponse;
+import com.ruta_ya.repository.EstadoUsuarioRepository;
 import com.ruta_ya.repository.MetodoPagoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +11,19 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Service
-public class MetodoPagoService {
+public class CatalogoService {
 
+    private final EstadoUsuarioRepository estadoUsuarioRepository;
     private final MetodoPagoRepository metodoPagoRepository;
+
+    public List<EstadoUsuarioResponse> getAllEstadosUsuario() {
+        try {
+            return estadoUsuarioRepository.getAll();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException("Error al obtener los estados de usuario");
+        }
+    }
 
     public List<MetodoPagoResponse> getAllMetodosPago() {
         try {
